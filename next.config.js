c023645aa8a968af7b833b8f425ab0df37aae3a1
Juani <juanechenique96@gmail.com>
2021-08-18 14:13:49 -0300

    subiendo a github pages (intento 3)

--- next.config.js
+++ next.config.js
@@ -1,32 +1,10 @@
 const debug = process.env.NODE_ENV !== "production";
-
 module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
-      "/about": { page: "/about" },
+      "/contact": { page: "/contact" },
     }
   },
-  //assetPrefix: '',
-  assetPrefix: !debug ? '/coin-tracker/' : '',
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map(rule => {
-      if (rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  }/*,
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // console.log('webpackDevMiddleware');
-    // console.log(config);
-    // Important: return the modified config
-    return config
-  }, */
+  assetPrefix: !debug ? 'https://juanechenique96.github.io/coin-tracker/' : '',
 }
