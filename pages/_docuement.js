6d0628ee664fce7f26fdc45bbdf6bb435672aba7
Juani <juanechenique96@gmail.com>
2021-08-19 09:29:52 -0300

    Agregado _document.js

---
+++ pages/_docuement.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name="description" content="Crypto coin tracker" />
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -10,10 +10,6 @@ export default function Contact() {
         <>
             <Head>
                 <title>Coin Tracker | Contact</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Navbar />
@@ -21,25 +17,26 @@ export default function Contact() {
                 <Footer />
 
                 <style jsx global>{`
-                * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                font-family: 'Poppins', sans-serif;
-                }
-                li {
-                list-style-type: none;
-                }
+                        * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                        font-family: 'Poppins', sans-serif;
+                        }
+                        li {
+                        list-style-type: none;
+                        }
 
-                a {
-                text-decoration: none;
-                }
-                
-                body {
-                overflow-x: hidden;
-                background-color: #F4F4F8;
-                }
-            `}</style>
+                        a {
+                        text-decoration: none;
+                        }
+                        
+                        body {
+                        overflow-x: hidden;
+                        background-color: #F4F4F8;
+                        }
+                    `}
+                </style>
             </body>
         </>)
 }
